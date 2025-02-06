58bf79a90da4fc426268b0aadb1995e9a8c8cb26
Bryan Vela <bryanedinson1998@hotmail.com>
2025-02-06 12:11:43 -0500

    Service comments

--- src/main/java/com/example/futbolcanchas/service/CanchaService.java
+++ src/main/java/com/example/futbolcanchas/service/CanchaService.java
@@ -5,6 +5,7 @@ import com.example.futbolcanchas.dto.CanchaDTO;
 import java.util.List;
 
 public interface CanchaService {
+    //Some Function to get info by Distrito
     List<CanchaDTO> getCanchasByDistrito(String distrito);
     CanchaDTO saveCancha(CanchaDTO canchaDTO);
 }
